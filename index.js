0cf59c830ba38ccf6f0f1a60e3517d46abc0b3fa
rafsun dipto <rafsundipto116@gmail.com>
2025-05-28 21:51:14 +0600

    post and get ok

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.get("/", (req, res) => {
 });
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://plant:4Hs80pLjXaoRqnYF@clusteralpha.2srbfo8.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAlpha";
+console.log(process.env.DB_USER, process.env.DB_PASS);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusteralpha.2srbfo8.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAlpha`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +27,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const plantDatabase = client.db("plantDatabase");
+    const plantCollection = plantDatabase.collection("plants");
+    // app post
+
+    app.post("/plants", async (req, res) => {
+      const newPlant = req.body;
+      console.log(newPlant);
+      const result = await plantCollection.insertOne(newPlant);
+      res.send(result);
+    });
+
+    // app get
+    app.get("/plants", async (req, res) => {
+      const cursor = plantCollection.find();
+      const result = await cursor.toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    // app patch
+
+    // app delete
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 2 });
     console.log("You successfully connected to MongoDB!");
