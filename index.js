227ffdac9b58b3e32e2b19b3f5b1ef1e9d56ab1a
rafsun dipto <rafsundipto116@gmail.com>
2025-07-31 18:57:30 +0600

    remove unnecessary code

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 console.log(process.env.DB_USER, process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clusteralpha.2srbfo8.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAlpha`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
